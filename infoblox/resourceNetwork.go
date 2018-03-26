7a6d9898dab7b139293c515b3dd2559f0d3246f7
saiprasannasastry <36291746+saiprasannasastry@users.noreply.github.com>
2018-03-26 17:07:24 +0530

    Validation of Cloud License (#8)
    Patch provides ability to read environment variables for
    provider info.
    
    Added a check to see NIOS have Cloud license or not.
    
    Renamed IPAddress resource to IPAllocation.go
    
    Fixed golint issues

--- infoblox/resourceNetwork.go
+++ infoblox/resourceNetwork.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/infobloxopen/infoblox-go-client"
+	"log"
 )
 
 func resourceNetwork() *schema.Resource {
@@ -18,84 +19,106 @@ func resourceNetwork() *schema.Resource {
 				Type:        schema.TypeString,
 				Optional:    true,
 				DefaultFunc: schema.EnvDefaultFunc("network_view_name", "default"),
-				Description: "Network view name available in NIOS Server",
+				Description: "Network view name available in NIOS Server.",
 			},
 			"network_name": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("network_name", nil),
-				Description: "The name of the network",
+				DefaultFunc: schema.EnvDefaultFunc("networkName", nil),
+				Description: "The name of the network.",
 			},
 			"cidr": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
 				DefaultFunc: schema.EnvDefaultFunc("net_address", nil),
-				Description: "Give the address in cidr format",
+				Description: "Give the address in cidr format.",
 			},
 			"tenant_id": &schema.Schema{
 				Type:        schema.TypeString,
 				Required:    true,
-				DefaultFunc: schema.EnvDefaultFunc("tenant_id", nil),
-				Description: "Unique identifier of your instance in cloud",
+				DefaultFunc: schema.EnvDefaultFunc("tenantID", nil),
+				Description: "Unique identifier of your instance in cloud.",
 			},
 		},
 	}
 }
 
 func resourceNetworkCreate(d *schema.ResourceData, m interface{}) error {
-	network_view_name := d.Get("network_view_name").(string)
+	log.Printf("[DEBUG] %s: Beginning network block Creation", resourceNetworkIDString(d))
+
+	networkViewName := d.Get("network_view_name").(string)
 	cidr := d.Get("cidr").(string)
-	network_name := d.Get("network_name").(string)
-	tenant_id := d.Get("tenant_id").(string)
+	networkName := d.Get("network_name").(string)
+	tenantID := d.Get("tenant_id").(string)
 	connector := m.(*ibclient.Connector)
 
-	objMgr := ibclient.NewObjectManager(connector, "terraform", tenant_id)
+	objMgr := ibclient.NewObjectManager(connector, "terraform", tenantID)
 
-	nwname, err := objMgr.CreateNetwork(network_view_name, cidr, network_name)
+	nwname, err := objMgr.CreateNetwork(networkViewName, cidr, networkName)
 	if err != nil {
-		return fmt.Errorf("Creation of network failed in network view (%s) : %s", network_view_name, err)
+		return fmt.Errorf("Creation of network failed in network view (%s) : %s", networkViewName, err)
 	}
 	d.SetId(nwname.Cidr)
 
+	log.Printf("[DEBUG] %s: Creation on network block complete", resourceNetworkIDString(d))
 	return nil
 }
 func resourceNetworkRead(d *schema.ResourceData, m interface{}) error {
-	network_view_name := d.Get("network_view_name").(string)
+	log.Printf("[DEBUG] %s: Reading the required network block", resourceNetworkIDString(d))
+
+	networkViewName := d.Get("network_view_name").(string)
 	cidr := d.Get("cidr").(string)
-	tenant_id := d.Get("tenant_id").(string)
+	tenantID := d.Get("tenant_id").(string)
 	connector := m.(*ibclient.Connector)
 
-	objMgr := ibclient.NewObjectManager(connector, "terraform", tenant_id)
+	objMgr := ibclient.NewObjectManager(connector, "terraform", tenantID)
 
-	_, err := objMgr.GetNetwork(network_view_name, cidr, nil)
+	_, err := objMgr.GetNetwork(networkViewName, cidr, nil)
 	if err != nil {
-		return fmt.Errorf("Getting Network from network view (%s) failed : %s", network_view_name, err)
+		return fmt.Errorf("Getting Network from network view (%s) failed : %s", networkViewName, err)
 	}
 
+	log.Printf("[DEBUG] %s: Completed reading network block", resourceNetworkIDString(d))
 	return nil
 }
 func resourceNetworkUpdate(d *schema.ResourceData, m interface{}) error {
 	//not supported by Infoblox Go Client for now
 	return nil
 }
+
 func resourceNetworkDelete(d *schema.ResourceData, m interface{}) error {
-	network_view_name := d.Get("network_view_name").(string)
+	log.Printf("[DEBUG] %s: Beginning Deletion of network block", resourceNetworkIDString(d))
+
+	networkViewName := d.Get("network_view_name").(string)
 	cidr := d.Get("cidr").(string)
-	tenant_id := d.Get("tenant_id").(string)
+	tenantID := d.Get("tenant_id").(string)
 	connector := m.(*ibclient.Connector)
 
-	objMgr := ibclient.NewObjectManager(connector, "terraform", tenant_id)
+	objMgr := ibclient.NewObjectManager(connector, "terraform", tenantID)
 
-	ref, err := objMgr.GetNetwork(network_view_name, cidr, nil)
+	ref, err := objMgr.GetNetwork(networkViewName, cidr, nil)
 	if err != nil {
-		return fmt.Errorf("Getting Network failed from network view(%s) for deletion : %s", network_view_name, err)
+		return fmt.Errorf("Getting Network failed from network view(%s) for deletion : %s", networkViewName, err)
 	}
 
 	_, err = objMgr.DeleteNetwork(ref.Ref, d.Get("network_view_name").(string))
 	if err != nil {
-		return fmt.Errorf("Deletion of Network failed from network view(%s) for deletion : %s", network_view_name, err)
+		return fmt.Errorf("Deletion of Network failed from network view(%s) for deletion : %s", networkViewName, err)
 	}
 	d.SetId("")
 
+	log.Printf("[DEBUG] %s: Deletion of network block complete", resourceNetworkIDString(d))
 	return nil
+}
+
+type resourceNetworkIDStringInterface interface {
+	Id() string
+}
+
+func resourceNetworkIDString(d resourceNetworkIDStringInterface) string {
+	id := d.Id()
+	if id == "" {
+		id = "<new resource>"
+	}
+	return fmt.Sprintf("infoblox_ip_allocation (ID = %s)", id)
 }
