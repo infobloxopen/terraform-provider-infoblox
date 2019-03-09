0caa8cd2dfdf5e3b310efed653c18e0fefc3a71c
saiprasannasastry <36291746+saiprasannasastry@users.noreply.github.com>
2019-03-09 10:31:52 +0530

    Update infoblox.tf

--- examples/VMware/HostRecord/infoblox.tf
+++ examples/VMware/HostRecord/infoblox.tf
@@ -1,8 +1,8 @@
-/*resource "infoblox_network_view" "demo_network_view"{
+resource "infoblox_network_view" "demo_network_view"{
   network_view_name="demo1"
   tenant_id="test"
 
-}*/
+}
 resource "infoblox_network" "demo_network"{
   network_view_name="demo1"
   network_name="ex1"
@@ -43,12 +43,12 @@ resource "infoblox_a_record" "demo_record"{
   zone="aa.com"
 tenant_id="test"
 }
-*/
-/*resource "infoblox_ptr_record" "demo_ptr"{
+
+resource "infoblox_ptr_record" "demo_ptr"{
 
   network_view_name="demo1"
   vm_name="${infoblox_ip_allocation.demo_allocation.vm_name}"
-  cidr="10.10.20.0/24"//"${infoblox_network.demo_network.cidr}"
+  cidr="${infoblox_network.demo_network.cidr}"
   ip_addr="${infoblox_ip_allocation.demo_allocation.ip_addr}"
   dns_view="default.demo1"
   zone="aa.com"
