4d076f9c64ec166e20b09398a348ad658d1a3f01
avrajath <rav@infoblox.com>
2020-06-18 04:28:05 -0400

    Adding DataSource for Network

---
+++ examples/datasources/azurerm/README.md
@@ -0,0 +1,14 @@
+## Example: Datasource Infoblox Network
+
+This example provisions the following Resources:
+
+- `infoblox_network`
+
+## Outputs
+
+1. ID
+2. Name
+
+## Usage
+
+- Refresh with `terraform apply`
---
+++ infoblox/datasource_infoblox_network_test.go
@@ -0,0 +1,36 @@
+package infoblox
+
+import (
+	"fmt"
+	"github.com/hashicorp/terraform/helper/resource"
+	"testing"
+)
+
+func TestAccDataSourceNetwork(t *testing.T) {
+
+	resource.Test(t, resource.TestCase{
+		PreCheck:  func() { testAccPreCheck(t) },
+		Providers: testAccProviders,
+		Steps: []resource.TestStep{
+			resource.TestStep{
+				Config: testAccDataSourceNetworkCreate,
+				Check: resource.ComposeTestCheckFunc(
+					resource.TestCheckResourceAttr("data.infoblox_network.acctest", "network_name", "acctest-network"),
+				),
+			},
+		},
+	})
+}
+
+var testAccDataSourceNetworkCreate = fmt.Sprintf(`
+resource "infoblox_network" "test_network"{
+  network_name      = "acctest-network"
+  cidr              = "10.4.20.0/24"
+  tenant_id         = "test_tenant_id"
+}
+
+data "infoblox_network" "acctest" {
+  cidr              = infoblox_network.test_network.cidr
+  tenant_id         = infoblox_network.test_network.tenant_id
+}
+`)
