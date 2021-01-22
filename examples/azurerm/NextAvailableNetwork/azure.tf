9889912cbadcfec08b6e59ece8cbb1cb1a90fe8d
somashekhar <somashekharvb@gmail.com>
2021-01-22 09:42:01 +0000

    tune the script

---
+++ examples/azurerm/NextAvailableNetwork/azure.tf
@@ -0,0 +1,96 @@
+# Provider details
+provider "azurerm" {
+    subscription_id = "xxx"
+    client_id       = "xxx"
+    client_secret   = "xxx"
+    tenant_id       = "xxx"
+}
+
+# create a resource group if it doesn't exist
+resource "azurerm_resource_group" "terraform" {
+    name = "tf-rg"
+    location = "ukwest"
+}
+
+# create virtual network
+resource "azurerm_virtual_network" "vnet" {
+    name = "tf-vnet"
+    address_space =  ["${infoblox_network.demo_network.cidr}"]
+    location = "ukwest"
+    resource_group_name = "${azurerm_resource_group.terraform.name}"
+}
+
+# create subnet
+resource "azurerm_subnet" "subnet" {
+    name = "tf-sub"
+    resource_group_name = "${azurerm_resource_group.terraform.name}"
+    virtual_network_name = "${azurerm_virtual_network.vnet.name}"
+    address_prefix ="10.0.0.0/24"
+    #network_security_group_id = "${azurerm_network_security_group.nsg.id}"
+}
+
+# create public IPs
+resource "azurerm_public_ip" "ip" {
+    name = "tf-ip"
+    location = "ukwest"
+    resource_group_name = "${azurerm_resource_group.terraform.name}"
+    public_ip_address_allocation = "dynamic"
+    domain_name_label = "a132"
+
+    tags {
+        environment = "staging"
+    }
+}
+
+# create network interface
+resource "azurerm_network_interface" "ni" {
+    name = "tf-ni"
+    location = "ukwest"
+    resource_group_name = "${azurerm_resource_group.terraform.name}"
+
+    ip_configuration {
+        name = "ipconfiguration"
+        subnet_id = "${azurerm_subnet.subnet.id}"
+        private_ip_address_allocation = "static"
+        private_ip_address ="${infoblox_ip_allocation.demo_allocation.ip_addr}"
+        public_ip_address_id = "${azurerm_public_ip.ip.id}"
+    }
+}
+
+# create virtual machine
+resource "azurerm_virtual_machine" "vm" {
+    name = "${infoblox_ip_allocation.demo_allocation.vm_name}"
+    location = "ukwest"
+    resource_group_name = "${azurerm_resource_group.terraform.name}"
+    network_interface_ids = ["${azurerm_network_interface.ni.id}"]
+    vm_size = "Standard_A6"
+
+    storage_image_reference {
+        publisher = "Canonical"
+        offer = "UbuntuServer"
+        sku = "16.04-LTS"
+        version = "latest"
+    }
+
+    storage_os_disk {
+        name = "myosdisk"
+        caching = "ReadWrite"
+        create_option = "FromImage"
+    }
+
+    os_profile {
+        computer_name  = "ubuntu"
+        admin_username = "ubuntu"
+        admin_password="ubuntu"
+    }
+
+    os_profile_linux_config {
+        disable_password_authentication = false
+           }
+
+    tags {
+        environment = "Terraform Demo"
+    }
+}
+
+
