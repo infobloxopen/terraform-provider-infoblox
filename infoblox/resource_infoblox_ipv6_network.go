cd18f25f36eee0699b8bcd70657c403e760778fb
somashekhar <somashekharvb@gmail.com>
2021-02-18 12:24:36 +0000

    PR changes

--- infoblox/resource_infoblox_ipv6_network.go
+++ infoblox/resource_infoblox_ipv6_network.go
@@ -2,7 +2,6 @@ package infoblox
 
 import (
 	"fmt"
-	"log"
 
 	ibclient "github.com/anagha-infoblox/infoblox-go-client"
 	"github.com/hashicorp/terraform/helper/schema"
@@ -64,7 +63,6 @@ func resourceIPv6Network() *schema.Resource {
 }
 
 func resourceIPv6NetworkCreate(d *schema.ResourceData, m interface{}) error {
-	log.Printf("[DEBUG] %s: Beginning IPv6 network block creation", resourceIPv6NetworkIDString(d))
 
 	networkViewName := d.Get("network_view_name").(string)
 	cidr := d.Get("cidr").(string)
@@ -75,7 +73,6 @@ func resourceIPv6NetworkCreate(d *schema.ResourceData, m interface{}) error {
 	prefixLen := d.Get("allocate_prefix_len").(int)
 
 	objMgr := ibclient.NewObjectManager(connector, "Terraform", tenantID)
-	//ea := make(ibclient.EA)
 
 	var IPv6Network *ibclient.IPv6Network
 	var err error
@@ -97,12 +94,10 @@ func resourceIPv6NetworkCreate(d *schema.ResourceData, m interface{}) error {
 
 	d.SetId(IPv6Network.Ref)
 
-	log.Printf("[DEBUG] %s: Creation on network block complete", resourceIPv6NetworkIDString(d))
-	return resourceIPv6NetworkRead(d, m)
+	return nil
 }
 
 func resourceIPv6NetworkRead(d *schema.ResourceData, m interface{}) error {
-	log.Printf("[DEBUG] %s: Reading the required network block", resourceIPv6NetworkIDString(d))
 
 	networkViewName := d.Get("network_view_name").(string)
 	tenantID := d.Get("tenant_id").(string)
@@ -114,12 +109,10 @@ func resourceIPv6NetworkRead(d *schema.ResourceData, m interface{}) error {
 		return fmt.Errorf("Getting Network block from network view (%s) failed : %s", networkViewName, err)
 	}
 	d.SetId(obj.Ref)
-	log.Printf("[DEBUG] %s: Completed reading network block", resourceNetworkIDString(d))
 	return nil
 }
 
 func resourceIPv6NetworkUpdate(d *schema.ResourceData, m interface{}) error {
-	log.Printf("[DEBUG] %s: Updating IPv6 network block EAs", resourceIPv6NetworkIDString(d))
 
 	networkViewName := d.Get("network_view_name").(string)
 	tenantID := d.Get("tenant_id").(string)
@@ -128,35 +121,32 @@ func resourceIPv6NetworkUpdate(d *schema.ResourceData, m interface{}) error {
 
 	objMgr := ibclient.NewObjectManager(connector, "Terraform", tenantID)
 
-	addEA := make(map[string]interface{})
-	removeEA := make(map[string]interface{})
+	addEA := make(ibclient.EA)
+	removeEA := make(ibclient.EA)
 
 	var err error
 	var IPv6Network *ibclient.IPv6Network
+
+	/* Pass value as nil along with key if want to remove that EA
+	   if want to add an EA then pass key value of the same */
 	for key, value := range extensibleAttributes {
 		if value == "nil" {
 			removeEA[key] = value
-			IPv6Network, err = objMgr.UpdateIPv6NetworkEA(d.Id(), addEA, removeEA)
-			if err != nil {
-				return fmt.Errorf("Deletion of IPv6 network EA failed from network view(%s): %s", networkViewName, err)
-			}
 		} else {
 			addEA[key] = value
-			IPv6Network, err = objMgr.UpdateIPv6NetworkEA(d.Id(), addEA, removeEA)
-			if err != nil {
-				return fmt.Errorf("Addition of IPv6 network EA failed from network view(%s): %s", networkViewName, err)
-			}
-
 		}
+		IPv6Network, err = objMgr.UpdateIPv6NetworkEA(d.Id(), addEA, removeEA)
+		if err != nil {
+			return fmt.Errorf("Updation of IPv6 network EA failed from network view(%s): %s", networkViewName, err)
+		}
+
 	}
 	d.SetId(IPv6Network.Ref)
 
-	log.Printf("[DEBUG] %s: Updation on IPv6 network block complete", resourceIPv6NetworkIDString(d))
-	return resourceIPv6NetworkRead(d, m)
+	return nil
 }
 
 func resourceIPv6NetworkDelete(d *schema.ResourceData, m interface{}) error {
-	log.Printf("[DEBUG] %s: Beginning Deletion of IPv6 network block", resourceIPv6NetworkIDString(d))
 
 	networkViewName := d.Get("network_view_name").(string)
 	tenantID := d.Get("tenant_id").(string)
@@ -167,20 +157,6 @@ func resourceIPv6NetworkDelete(d *schema.ResourceData, m interface{}) error {
 	if err != nil {
 		return fmt.Errorf("Deletion of IPv6 Network block failed from network view(%s): %s", networkViewName, err)
 	}
-	d.SetId("")
 
-	log.Printf("[DEBUG] %s: Deletion of IPv6 network block complete", resourceIPv6NetworkIDString(d))
 	return nil
-}
-
-type resourceIPv6NetworkIDStringInterface interface {
-	Id() string
-}
-
-func resourceIPv6NetworkIDString(d resourceIPv6NetworkIDStringInterface) string {
-	id := d.Id()
-	if id == "" {
-		id = "<new resource>"
-	}
-	return fmt.Sprintf("infobloxIPv6Network (ID = %s)", id)
 }
