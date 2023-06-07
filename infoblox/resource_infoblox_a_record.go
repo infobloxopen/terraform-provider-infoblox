f08fe0751f2337b0cd6ab12dd66aff7a76929023
hemanthKa677 <hkumar3@infoblox.com>
2023-06-07 09:13:59 +0530

    Fixes test case failure in A record

--- infoblox/resource_infoblox_a_record.go
+++ infoblox/resource_infoblox_a_record.go
@@ -262,10 +262,6 @@ func resourceARecordUpdate(d *schema.ResourceData, m interface{}) error {
 		cidr = ""
 	}
 
-	if !d.HasChange("cidr") && d.HasChange("ip_addr") {
-		return fmt.Errorf("allowing 'ip_addr' to update creates inconsistency with 'cidr'")
-	}
-
 	// this will fix issue when 'cidr' given for update when 'ip_addr' already
 	// exists in the state, unable to update value of ip_addr in NIOS with given 'cidr'.
 	if d.HasChange("cidr") && ipAddr != "" {
