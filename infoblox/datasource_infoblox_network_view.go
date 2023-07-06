56bf6c5411c9433d2260b551cbd6799a81985fbe
Aleksei Chernevskii <aleksei_chernevskii@epam.com>
2023-07-06 12:19:33 -0500

    Changed network view data source
    * Rewrote read schema and logic for network view data
      source. Search by extensible attributes is now available
      for this data source.

--- infoblox/datasource_infoblox_network_view.go
+++ infoblox/datasource_infoblox_network_view.go
@@ -1,8 +1,12 @@
 package infoblox
 
 import (
+	"context"
 	"encoding/json"
 	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"strconv"
+	"time"
 
 	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
 	ibclient "github.com/infobloxopen/infoblox-go-client/v2"
@@ -10,58 +14,108 @@ import (
 
 func dataSourceNetworkView() *schema.Resource {
 	return &schema.Resource{
-		Read: dataSourceNetworkViewRead,
+		ReadContext: dataSourceNetworkViewRead,
 		Schema: map[string]*schema.Schema{
-			"name": {
-				Type:        schema.TypeString,
-				Required:    true,
-				Description: "Name of the view shown in NIOS's UI.",
+			"filters": {
+				Type:     schema.TypeMap,
+				Required: true,
 			},
-			"comment": {
-				Type:        schema.TypeString,
+
+			"results": {
+				Type:        schema.TypeList,
 				Computed:    true,
-				Description: "Network view's description",
-			},
-			"ext_attrs": {
-				Type:        schema.TypeString,
-				Computed:    true,
-				Description: "The Extensible attributes of the network view.",
+				Description: "List of network views matching filters.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"name": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Name of the view shown in NIOS's UI.",
+						},
+						"comment": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Network view's description",
+						},
+						"ext_attrs": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The Extensible attributes of the network view.",
+						},
+					},
+				},
 			},
 		},
 	}
 }
 
-func dataSourceNetworkViewRead(d *schema.ResourceData, m interface{}) error {
-	name := d.Get("name").(string)
+func dataSourceNetworkViewRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	var diags diag.Diagnostics
 
 	connector := m.(ibclient.IBConnector)
-	objMgr := ibclient.NewObjectManager(connector, "Terraform", "")
-	obj, err := objMgr.GetNetworkView(name)
+
+	n := &ibclient.NetworkView{}
+	n.SetReturnFields(append(n.ReturnFields(), "extattrs"))
+	filters := filterFromMap(d.Get("filters").(map[string]interface{}))
+	qp := ibclient.NewQueryParams(false, filters)
+	var res []ibclient.NetworkView
+
+	err := connector.GetObject(n, "", qp, &res)
 	if err != nil {
-		return fmt.Errorf("getting network view '%s' failed: %s", name, err)
+		return diag.FromErr(fmt.Errorf("getting network view failed: %s", err))
 	}
 
 	// TODO: temporary scaffold, need to rework marshalling/unmarshalling of EAs
 	//       (avoiding additional layer of keys ("value" key)
+	results := make([]interface{}, 0, len(res))
+	for _, n := range res {
+		networkViewFlat, err := flattenNetworkView(n)
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("failed to flatten network view: %w", err))
+		}
+
+		results = append(results, networkViewFlat)
+	}
+
+	err = d.Set("results", results)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// always run
+	d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
+
+	return diags
+}
+
+func flattenNetworkView(nv ibclient.NetworkView) (map[string]interface{}, error) {
 	var eaMap map[string]interface{}
-	if obj.Ea != nil && len(obj.Ea) > 0 {
-		eaMap = (map[string]interface{})(obj.Ea)
+	if nv.Ea != nil && len(nv.Ea) > 0 {
+		eaMap = (map[string]interface{})(nv.Ea)
 	} else {
 		eaMap = make(map[string]interface{})
 	}
 	ea, err := json.Marshal(eaMap)
 	if err != nil {
-		return err
-	}
-	if err = d.Set("ext_attrs", string(ea)); err != nil {
-		return err
+		return nil, err
 	}
 
-	if err := d.Set("comment", obj.Comment); err != nil {
-		return err
+	res := map[string]interface{}{
+		"id":        nv.Ref,
+		"ext_attrs": string(ea),
 	}
 
-	d.SetId(obj.Ref)
+	if nv.Name != nil {
+		res["name"] = *nv.Name
+	}
 
-	return nil
+	if nv.Comment != nil {
+		res["comment"] = *nv.Comment
+	}
+
+	return res, nil
 }
