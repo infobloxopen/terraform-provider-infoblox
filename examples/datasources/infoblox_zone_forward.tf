d1bd466433ee68218e8e1d201de55755de70f1ad
Aish-sp <163982471+Aish-sp@users.noreply.github.com>
2024-06-19 19:42:34 +0530

    Update infoblox_zone_forward.tf

--- examples/datasources/infoblox_zone_forward.tf
+++ examples/datasources/infoblox_zone_forward.tf
@@ -1,12 +1,51 @@
+resource "infoblox_zone_forward" "forward_zone10" {
+  fqdn = "zone_forward.ex.org"
+  forward_to {
+    name = "test22.dz.ex.com"
+    address = "10.0.0.1"
+  }
+  forward_to {
+    name = "test2.dz.ex.com"
+    address = "10.0.0.2"
+  }
+}
+
 // accessing Zone Forward by specifying fqdn and view
 data "infoblox_zone_forward" "data_zone_foward" {
   filters = {
-    fqdn = "test.ex.com"
-    view = "nondefault_view"
+    fqdn = "zone_forward.ex.org"
+    view = "default"
   }
+  // This is just to ensure that the record has been be created
+  depends_on = [infoblox_zone_forward.forward_zone10]
 }
 
 // returns matching Zone Forward with fqdn and view, if any
 output "zone_forward_data3" {
   value = data.infoblox_zone_forward.data_zone_foward
+}
+
+
+resource "infoblox_zone_forward" "forward_zone11" {
+  fqdn = "195.1.0.0/24"
+  comment = "Forward zone IPV4"
+  external_ns_group = "stub server"
+  zone_format = "IPV4"
+  ns_group = "test"
+}
+
+// accessing Zone Forward by specifying fqdn, view and comment
+data "infoblox_zone_forward" "data_zone_foward2" {
+  filters = {
+    fqdn = "195.1.0.0/24"
+    view = "default"
+    comment = "Forward zone IPV4"
+  }
+  // This is just to ensure that the record has been be created
+  depends_on = [infoblox_zone_forward.forward_zone11]
+}
+
+// returns matching Zone Forward with fqdn, view and comment, if any
+output "zone_forward_data4" {
+  value = data.infoblox_zone_forward.data_zone_foward2
 }
