0723cc043aeba83ffdfb43c5060cbbf6f0a5bab7
Chaithra001 <chaithra@infoblox.com>
2025-03-16 17:13:38 +0530

    Implementation for fixed address

---
+++ infoblox/datasource_infoblox_fixed_address.go
@@ -0,0 +1,212 @@
+package infoblox
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/diag"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	ibclient "github.com/infobloxopen/infoblox-go-client/v2"
+	"strconv"
+	"time"
+)
+
+func dataSourceFixedAddress() *schema.Resource {
+	return &schema.Resource{
+		ReadContext: dataSourceFixedAddressRead,
+		Schema: map[string]*schema.Schema{
+			"filters": {
+				Type:     schema.TypeMap,
+				Required: true,
+			},
+
+			"results": {
+				Type:        schema.TypeList,
+				Computed:    true,
+				Description: "List of fixed address matching filters",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"id": {
+							Type:     schema.TypeString,
+							Computed: true,
+						},
+						"agent_circuit_id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The agent circuit ID for the fixed address.",
+						},
+						"agent_remote_id": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The agent remote ID for the fixed address.",
+						},
+						"client_identifier_prepend_zero": {
+							Type:        schema.TypeBool,
+							Computed:    true,
+							Description: "This field controls whether there is a prepend for the dhcp-client-identifier of a fixed address.",
+						},
+						"comment": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Comment for the fixed address; maximum 256 characters.",
+						},
+						"dhcp_client_identifier": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The DHCP client ID for the fixed address. The field is required only when match_client is set to CLIENT_ID.",
+						},
+						"disable": {
+							Type:        schema.TypeBool,
+							Computed:    true,
+							Description: "Determines whether a fixed address is disabled or not. When this is set to False, the fixed address is enabled.",
+						},
+						"ext_attrs": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "Extensible attributes of the A-record to be added/updated, as a map in JSON format",
+						},
+						"ipv4addr": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The IPv4 Address of the fixed address.",
+						},
+						"mac": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The MAC address value for this fixed address.",
+						},
+						"match_client": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The match client for the fixed address.Valid values are CIRCUIT_ID, CLIENT_ID , MAC_ADDRESS, REMOTE_ID and RESERVED",
+						},
+						"name": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "This field contains the name of this fixed address.",
+						},
+						"network": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The network to which this fixed address belongs, in IPv4 Address/CIDR format.",
+						},
+						"network_view": {
+							Type:        schema.TypeString,
+							Computed:    true,
+							Description: "The name of the network view in which this fixed address resides.",
+						},
+						"options": {
+							Type:        schema.TypeList,
+							Computed:    true,
+							Description: "An array of DHCP option structs that lists the DHCP options associated with the object.",
+							Elem: &schema.Resource{
+								Schema: map[string]*schema.Schema{
+									"name": {
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "The name of the DHCP option.",
+									},
+									"num": {
+										Type:        schema.TypeInt,
+										Computed:    true,
+										Description: "The code of the DHCP option.",
+									},
+									"use_option": {
+										Type:        schema.TypeBool,
+										Computed:    true,
+										Description: "Only applies to special options that are displayed separately from other options and have a use flag.",
+									},
+									"value": {
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "Value of the DHCP option",
+									},
+									"vendor_class": {
+										Type:        schema.TypeString,
+										Computed:    true,
+										Description: "The name of the space this DHCP option is associated to.",
+									},
+								},
+							},
+						},
+						"use_option": {
+							Type:        schema.TypeBool,
+							Computed:    true,
+							Description: "Use option is a flag that indicates whether the options field are used or not.",
+						},
+					},
+				},
+			},
+		},
+	}
+}
+
+func dataSourceFixedAddressRead(ctx context.Context, d *schema.ResourceData, m interface{}) diag.Diagnostics {
+	connector := m.(ibclient.IBConnector)
+
+	var diags diag.Diagnostics
+
+	filters := filterFromMap(d.Get("filters").(map[string]interface{}))
+	objMgr := ibclient.NewObjectManager(connector, "Terraform", "")
+
+	qp := ibclient.NewQueryParams(false, filters)
+	res, err := objMgr.GetAllFixedAddress(qp, false)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+	if res == nil {
+		return diag.FromErr(fmt.Errorf("API returns a nil/empty ID for Fixed address "))
+	}
+	results := make([]interface{}, 0, len(res))
+	for _, r := range res {
+		fixedAddress, err := flattenFixedAddress(r)
+		if err != nil {
+			return diag.FromErr(fmt.Errorf("failed to flatten Fixed address : %w", err))
+		}
+		results = append(results, fixedAddress)
+	}
+
+	err = d.Set("results", results)
+	if err != nil {
+		return diag.FromErr(err)
+	}
+
+	// always run
+	d.SetId(strconv.FormatInt(time.Now().Unix(), 10))
+
+	return diags
+}
+
+func flattenFixedAddress(fixedAddress ibclient.FixedAddress) (map[string]interface{}, error) {
+	var eaMap map[string]interface{}
+	if fixedAddress.Ea != nil && len(fixedAddress.Ea) > 0 {
+		eaMap = fixedAddress.Ea
+	} else {
+		eaMap = make(map[string]interface{})
+	}
+
+	ea, err := json.Marshal(eaMap)
+	if err != nil {
+		return nil, err
+	}
+	res := map[string]interface{}{
+		"id":                             fixedAddress.Ref,
+		"network_view":                   fixedAddress.NetviewName,
+		"network":                        fixedAddress.Cidr,
+		"comment":                        fixedAddress.Comment,
+		"ipv4addr":                       fixedAddress.IPv4Address,
+		"mac":                            fixedAddress.Mac,
+		"match_client":                   fixedAddress.MatchClient,
+		"agent_circuit_id":               fixedAddress.AgentCircuitId,
+		"agent_remote_id":                fixedAddress.AgentRemoteId,
+		"client_identifier_prepend_zero": fixedAddress.ClientIdentifierPrependZero,
+		"use_option":                     fixedAddress.UseOptions,
+		"disable":                        fixedAddress.Disable,
+		"dhcp_client_identifier":         fixedAddress.DhcpClientIdentifier,
+		"ext_attrs":                      string(ea),
+	}
+	if fixedAddress.Options != nil {
+		res["options"] = convertDhcpOptionsToInterface(fixedAddress.Options)
+	}
+	return res, nil
+}
---
+++ infoblox/resource_infoblox_fixed_address.go
@@ -0,0 +1,596 @@
+package infoblox
+
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	ibclient "github.com/infobloxopen/infoblox-go-client/v2"
+)
+
+func resourceFixedRecord() *schema.Resource {
+	return &schema.Resource{
+		Create: resourceFixedRecordCreate,
+		Read:   resourceFixedRecordRead,
+		Update: resourceFixedRecordUpdate,
+		Delete: resourceFixedRecordDelete,
+		Importer: &schema.ResourceImporter{
+			State: resourceFixedRecordImport,
+		},
+		CustomizeDiff: func(context context.Context, d *schema.ResourceDiff, meta interface{}) error {
+			if internalID := d.Get("internal_id"); internalID == "" || internalID == nil {
+				err := d.SetNewComputed("internal_id")
+				if err != nil {
+					return err
+				}
+			}
+			return nil
+		},
+		Schema: map[string]*schema.Schema{
+			"agent_circuit_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The agent circuit ID for the fixed address.",
+			},
+			"agent_remote_id": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The agent remote ID for the fixed address.",
+			},
+			"client_identifier_prepend_zero": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "This field controls whether there is a prepend for the dhcp-client-identifier of a fixed address.",
+			},
+			"comment": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "Comment for the fixed address; maximum 256 characters.",
+			},
+			"dhcp_client_identifier": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The DHCP client ID for the fixed address. The field is required only when match_client is set to CLIENT_ID.",
+			},
+			"disable": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "Determines whether a fixed address is disabled or not. When this is set to False, the fixed address is enabled.",
+			},
+			"ext_attrs": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "",
+				Description: "Extensible attributes of the A-record to be added/updated, as a map in JSON format",
+			},
+			"ipv4addr": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The IPv4 Address of the fixed address.",
+				DiffSuppressFunc: func(k, oldValue, newValue string, d *schema.ResourceData) bool {
+					oldNetwork, _ := d.GetChange("network")
+					if oldValue != "" && newValue == "" && oldNetwork != "" {
+						return true
+					}
+					return false
+				},
+			},
+			"mac": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The MAC address value for this fixed address.",
+				DiffSuppressFunc: func(k, oldValue, newValue string, d *schema.ResourceData) bool {
+					if d.Get("match_client").(string) != "MAC_ADDRESS" && newValue != "" {
+						return true
+					}
+					return false
+				},
+			},
+			"match_client": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     "MAC_ADDRESS",
+				Description: "The match client for the fixed address.Valid values are CIRCUIT_ID, CLIENT_ID , MAC_ADDRESS, REMOTE_ID and RESERVED",
+			},
+			"name": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "This field contains the name of this fixed address.",
+			},
+			"network": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Description: "The network to which this fixed address belongs, in IPv4 Address/CIDR format.",
+				DiffSuppressFunc: func(k, old, new string, d *schema.ResourceData) bool {
+					if d.Get("ipv4addr").(string) != "" && new == "" {
+						return true
+					}
+					return false
+				},
+			},
+			"network_view": {
+				Type:        schema.TypeString,
+				Optional:    true,
+				Default:     defaultNetView,
+				Description: "The name of the network view in which this fixed address resides.",
+			},
+			"options": {
+				Type:        schema.TypeList,
+				Optional:    true,
+				Description: "An array of DHCP option structs that lists the DHCP options associated with the object.",
+				Elem: &schema.Resource{
+					Schema: map[string]*schema.Schema{
+						"name": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "The name of the DHCP option.",
+						},
+						"num": {
+							Type:        schema.TypeInt,
+							Optional:    true,
+							Description: "The code of the DHCP option.",
+						},
+						"use_option": {
+							Type:        schema.TypeBool,
+							Optional:    true,
+							Description: "Only applies to special options that are displayed separately from other options and have a use flag.",
+						},
+						"value": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "Value of the DHCP option",
+						},
+						"vendor_class": {
+							Type:        schema.TypeString,
+							Optional:    true,
+							Description: "The name of the space this DHCP option is associated to.",
+						},
+					},
+				},
+				DiffSuppressFunc: func(k, oldValue, newValue string, d *schema.ResourceData) bool {
+					if newValue == "0" {
+						return false
+					}
+					oldList, newList := d.GetChange("options")
+					return CompareSortedList(oldList, newList, "name", "num")
+				},
+			},
+			"use_option": {
+				Type:        schema.TypeBool,
+				Optional:    true,
+				Default:     false,
+				Description: "Use option is a flag that indicates whether the options field are used or not.",
+			},
+			"internal_id": {
+				Type:     schema.TypeString,
+				Computed: true,
+				Description: "Internal ID of an object at NIOS side," +
+					" used by Infoblox Terraform plugin to search for a NIOS's object" +
+					" which corresponds to the Terraform resource.",
+			},
+			"ref": {
+				Type:        schema.TypeString,
+				Computed:    true,
+				Description: "NIOS object's reference, not to be set by a user.",
+			},
+		},
+	}
+}
+func resourceFixedRecordCreate(d *schema.ResourceData, m interface{}) error {
+	// Check if internal_id is set manually
+	if intId := d.Get("internal_id"); intId.(string) != "" {
+		return fmt.Errorf("the value of 'internal_id' field must not be set manually")
+	}
+	agentCircuitId := d.Get("agent_circuit_id").(string)
+	agentRemoteId := d.Get("agent_remote_id").(string)
+	clientIdentifierPrependZero := d.Get("client_identifier_prepend_zero").(bool)
+	comment := d.Get("comment").(string)
+	dhcpClientIdentifier := d.Get("dhcp_client_identifier").(string)
+	disable := d.Get("disable").(bool)
+	ipAddr := d.Get("ipv4addr").(string)
+	mac := d.Get("mac").(string)
+	matchClient := d.Get("match_client").(string)
+	name := d.Get("name").(string)
+	network := d.Get("network").(string)
+	networkView := d.Get("network_view").(string)
+
+	optionsInterface := d.Get("options").([]interface{})
+	options := ConvertInterfaceToDhcpOptions(optionsInterface)
+	useOptions := d.Get("use_option").(bool)
+	extAttrJSON := d.Get("ext_attrs").(string)
+	extAttrs, err := terraformDeserializeEAs(extAttrJSON)
+	if err != nil {
+		return err
+	}
+
+	// Generate internal ID and add it to the extensible attributes
+	internalId := generateInternalId()
+	extAttrs[eaNameForInternalId] = internalId.String()
+
+	var tenantID string
+	if tempVal, found := extAttrs[eaNameForTenantId]; found {
+		tenantID = tempVal.(string)
+	}
+
+	connector := m.(ibclient.IBConnector)
+	objMgr := ibclient.NewObjectManager(connector, "Terraform", tenantID)
+
+	fixedAddress, err := objMgr.AllocateIP(networkView, network, ipAddr, false, mac, name, comment, extAttrs, matchClient, agentCircuitId, agentRemoteId, clientIdentifierPrependZero, dhcpClientIdentifier, disable, options, useOptions)
+	if err != nil {
+		return err
+	}
+	d.SetId(fixedAddress.Ref)
+	if err = d.Set("internal_id", internalId.String()); err != nil {
+		return err
+	}
+	if err = d.Set("ref", fixedAddress.Ref); err != nil {
+		return err
+	}
+	return resourceFixedRecordRead(d, m)
+}
+func resourceFixedRecordRead(d *schema.ResourceData, m interface{}) error {
+	extAttrJSON := d.Get("ext_attrs").(string)
+	extAttrs, err := terraformDeserializeEAs(extAttrJSON)
+	if err != nil {
+		return err
+	}
+
+	rec, err := searchObjectByRefOrInternalId("FixedAddress", d, m)
+	if err != nil {
+		if _, ok := err.(*ibclient.NotFoundError); !ok {
+			return ibclient.NewNotFoundError(fmt.Sprintf(
+				"cannot find appropriate object on NIOS side for resource with ID '%s': %s;", d.Id(), err))
+		} else {
+			d.SetId("")
+			return nil
+		}
+	}
+	var fixedAddress *ibclient.FixedAddress
+	recJson, err := json.Marshal(rec)
+	if err != nil {
+		return fmt.Errorf("failed to marshal fixed address : %s", err.Error())
+	}
+	err = json.Unmarshal(recJson, &fixedAddress)
+	if err != nil {
+		return fmt.Errorf("failed getting fixed address : %s", err.Error())
+	}
+
+	delete(fixedAddress.Ea, eaNameForInternalId)
+	omittedEAs := omitEAs(fixedAddress.Ea, extAttrs)
+
+	if omittedEAs != nil && len(omittedEAs) > 0 {
+		eaJSON, err := terraformSerializeEAs(omittedEAs)
+		if err != nil {
+			return err
+		}
+		if err = d.Set("ext_attrs", eaJSON); err != nil {
+			return err
+		}
+	}
+	if err = d.Set("comment", fixedAddress.Comment); err != nil {
+		return err
+	}
+	if err = d.Set("disable", fixedAddress.Disable); err != nil {
+		return err
+	}
+	if err = d.Set("ipv4addr", fixedAddress.IPv4Address); err != nil {
+		return err
+	}
+	if err = d.Set("mac", fixedAddress.Mac); err != nil {
+		return err
+	}
+	if err = d.Set("match_client", fixedAddress.MatchClient); err != nil {
+		return err
+	}
+	if err = d.Set("name", fixedAddress.Name); err != nil {
+		return err
+	}
+	if err = d.Set("network", fixedAddress.Cidr); err != nil {
+		return err
+	}
+	if err = d.Set("network_view", fixedAddress.NetviewName); err != nil {
+		return err
+	}
+
+	if err = d.Set("agent_circuit_id", fixedAddress.AgentCircuitId); err != nil {
+		return err
+	}
+	if err = d.Set("agent_remote_id", fixedAddress.AgentRemoteId); err != nil {
+		return err
+	}
+	if err = d.Set("client_identifier_prepend_zero", fixedAddress.ClientIdentifierPrependZero); err != nil {
+		return err
+	}
+	if err = d.Set("dhcp_client_identifier", fixedAddress.DhcpClientIdentifier); err != nil {
+		return err
+	}
+	if err = d.Set("use_option", fixedAddress.UseOptions); err != nil {
+		return err
+	}
+	if err = d.Set("options", convertDhcpOptionsToInterface(fixedAddress.Options)); err != nil {
+		return err
+	}
+	d.SetId(fixedAddress.Ref)
+	return nil
+}
+func resourceFixedRecordUpdate(d *schema.ResourceData, m interface{}) error {
+	var updateSuccessful bool
+	defer func() {
+		if !updateSuccessful {
+			// Reverting the state back, in case of a failure,
+			// otherwise Terraform will keep the values, which leaded to the failure,
+			// in the state file.
+			prevComment, _ := d.GetChange("comment")
+			prevDisable, _ := d.GetChange("disable")
+			prevIpv4addr, _ := d.GetChange("ipv4addr")
+			prevMac, _ := d.GetChange("mac")
+			prevMatchClient, _ := d.GetChange("match_client")
+			prevName, _ := d.GetChange("name")
+			prevNetwork, _ := d.GetChange("network")
+			prevNetworkView, _ := d.GetChange("network_view")
+			prevAgentCircuitId, _ := d.GetChange("agent_circuit_id")
+			prevAgentRemoteId, _ := d.GetChange("agent_remote_id")
+			prevClientIdentifierPrependZero, _ := d.GetChange("client_identifier_prepend_zero")
+			prevDhcpClientIdentifier, _ := d.GetChange("dhcp_client_identifier")
+			prevUseOption, _ := d.GetChange("use_option")
+			prevOptions, _ := d.GetChange("options")
+			prevExtAttrsJSON, _ := d.GetChange("ext_attrs")
+
+			_ = d.Set("comment", prevComment.(string))
+			_ = d.Set("disable", prevDisable.(bool))
+			_ = d.Set("ipv4addr", prevIpv4addr.(string))
+			_ = d.Set("mac", prevMac.(string))
+			_ = d.Set("match_client", prevMatchClient.(string))
+			_ = d.Set("name", prevName.(string))
+			_ = d.Set("network", prevNetwork.(string))
+			_ = d.Set("network_view", prevNetworkView.(string))
+			_ = d.Set("agent_circuit_id", prevAgentCircuitId.(string))
+			_ = d.Set("agent_remote_id", prevAgentRemoteId.(string))
+			_ = d.Set("client_identifier_prepend_zero", prevClientIdentifierPrependZero.(bool))
+			_ = d.Set("dhcp_client_identifier", prevDhcpClientIdentifier.(string))
+			_ = d.Set("use_option", prevUseOption.(bool))
+			_ = d.Set("options", prevOptions)
+			_ = d.Set("ext_attrs", prevExtAttrsJSON.(string))
+
+		}
+	}()
+	if d.HasChange("internal_id") {
+		return fmt.Errorf("changing the value of 'internal_id' field is not allowed")
+	}
+	if d.HasChange("network_view") {
+		return fmt.Errorf("changing the value of 'network_view' field is not allowed")
+	}
+
+	network := d.Get("network").(string)
+	ipv4addr := d.Get("ipv4addr").(string)
+	mac := d.Get("mac").(string)
+	name := d.Get("name").(string)
+	comment := d.Get("comment").(string)
+	disable := d.Get("disable").(bool)
+	matchClient := d.Get("match_client").(string)
+	agentCircuitId := d.Get("agent_circuit_id").(string)
+	agentRemoteId := d.Get("agent_remote_id").(string)
+	clientIdentifierPrependZero := d.Get("client_identifier_prepend_zero").(bool)
+	dhcpClientIdentifier := d.Get("dhcp_client_identifier").(string)
+	useOptions := d.Get("use_option").(bool)
+	optionsInterface := d.Get("options").([]interface{})
+	options := ConvertInterfaceToDhcpOptions(optionsInterface)
+	oldExtAttrsJSON, newExtAttrsJSON := d.GetChange("ext_attrs")
+
+	newExtAttrs, err := terraformDeserializeEAs(newExtAttrsJSON.(string))
+	if err != nil {
+		return err
+	}
+
+	oldExtAttrs, err := terraformDeserializeEAs(oldExtAttrsJSON.(string))
+	if err != nil {
+		return err
+	}
+
+	var tenantID string
+	if tempVal, found := newExtAttrs[eaNameForTenantId]; found {
+		tenantID = tempVal.(string)
+	}
+	connector := m.(ibclient.IBConnector)
+	objMgr := ibclient.NewObjectManager(connector, "Terraform", tenantID)
+
+	// Get by Ref
+	recA, err := objMgr.GetFixedAddressByRef(d.Id())
+	if err != nil {
+		return fmt.Errorf("failed to read Fixed address for update operation: %w", err)
+	}
+
+	// If 'internal_id' is not set, then generate a new one and set it to the EA.
+	internalId := d.Get("internal_id").(string)
+	if internalId == "" {
+		internalId = generateInternalId().String()
+	}
+	newInternalId := newInternalResourceIdFromString(internalId)
+	newExtAttrs[eaNameForInternalId] = newInternalId.String()
+
+	newExtAttrs, err = mergeEAs(recA.Ea, newExtAttrs, oldExtAttrs, connector)
+
+	obj, err := objMgr.UpdateFixedAddress(
+		d.Id(),
+		"",
+		name,
+		network,
+		ipv4addr,
+		matchClient,
+		mac,
+		comment,
+		newExtAttrs,
+		agentCircuitId,
+		agentRemoteId,
+		clientIdentifierPrependZero,
+		dhcpClientIdentifier,
+		disable,
+		options,
+		useOptions,
+	)
+	if err != nil {
+		return fmt.Errorf("error updating Fixed address: %w", err)
+	}
+	updateSuccessful = true
+	d.SetId(obj.Ref)
+	if err = d.Set("ref", obj.Ref); err != nil {
+		return err
+	}
+	if err = d.Set("internal_id", newInternalId.String()); err != nil {
+		return err
+	}
+	return resourceFixedRecordRead(d, m)
+}
+func resourceFixedRecordDelete(d *schema.ResourceData, m interface{}) error {
+	extAttrJSON := d.Get("ext_attrs").(string)
+	extAttrs, err := terraformDeserializeEAs(extAttrJSON)
+	if err != nil {
+		return err
+	}
+
+	var tenantID string
+	tempVal, found := extAttrs[eaNameForTenantId]
+	if found {
+		tenantID = tempVal.(string)
+	}
+
+	connector := m.(ibclient.IBConnector)
+	objMgr := ibclient.NewObjectManager(connector, "Terraform", tenantID)
+
+	rec, err := searchObjectByRefOrInternalId("FixedAddress", d, m)
+	if err != nil {
+		if _, ok := err.(*ibclient.NotFoundError); !ok {
+			return ibclient.NewNotFoundError(fmt.Sprintf(
+				"cannot find appropriate object on NIOS side for resource with ID '%s': %s;", d.Id(), err))
+		} else {
+			d.SetId("")
+			return nil
+		}
+	}
+
+	// Assertion of object type and error handling
+	var fixedAddress *ibclient.FixedAddress
+	recJson, _ := json.Marshal(rec)
+	err = json.Unmarshal(recJson, &fixedAddress)
+
+	_, err = objMgr.DeleteARecord(fixedAddress.Ref)
+	if err != nil {
+		return fmt.Errorf("deletion of Fixed address failed: %w", err)
+	}
+	d.SetId("")
+
+	return nil
+}
+
+func resourceFixedRecordImport(d *schema.ResourceData, m interface{}) ([]*schema.ResourceData, error) {
+	extAttrJSON := d.Get("ext_attrs").(string)
+	extAttrs, err := terraformDeserializeEAs(extAttrJSON)
+	if err != nil {
+		return nil, err
+	}
+
+	var tenantID string
+	tempVal, found := extAttrs[eaNameForTenantId]
+	if found {
+		tenantID = tempVal.(string)
+	}
+
+	connector := m.(ibclient.IBConnector)
+	objMgr := ibclient.NewObjectManager(connector, "Terraform", tenantID)
+
+	obj, err := objMgr.GetFixedAddressByRef(d.Id())
+	if err != nil {
+		return nil, fmt.Errorf("failed getting fixed address: %w", err)
+	}
+	if err = d.Set("comment", obj.Comment); err != nil {
+		return nil, err
+	}
+	if err = d.Set("disable", obj.Disable); err != nil {
+		return nil, err
+	}
+	if obj.Ea != nil && len(obj.Ea) > 0 {
+		eaJSON, err := terraformSerializeEAs(obj.Ea)
+		if err != nil {
+			return nil, err
+		}
+		if err = d.Set("ext_attrs", eaJSON); err != nil {
+			return nil, err
+		}
+	}
+	if err = d.Set("ipv4addr", obj.IPv4Address); err != nil {
+		return nil, err
+	}
+	if err = d.Set("mac", obj.Mac); err != nil {
+		return nil, err
+	}
+	if err = d.Set("match_client", obj.MatchClient); err != nil {
+		return nil, err
+	}
+	if err = d.Set("name", obj.Name); err != nil {
+		return nil, err
+	}
+	if err = d.Set("agent_circuit_id", obj.AgentCircuitId); err != nil {
+		return nil, err
+	}
+	if err = d.Set("agent_remote_id", obj.AgentRemoteId); err != nil {
+		return nil, err
+	}
+	if err = d.Set("network", obj.Cidr); err != nil {
+		return nil, err
+	}
+	if err = d.Set("network_view", obj.NetviewName); err != nil {
+		return nil, err
+	}
+	if err = d.Set("client_identifier_prepend_zero", obj.ClientIdentifierPrependZero); err != nil {
+		return nil, err
+	}
+	if err = d.Set("dhcp_client_identifier", obj.DhcpClientIdentifier); err != nil {
+		return nil, err
+	}
+	if err = d.Set("use_option", obj.UseOptions); err != nil {
+		return nil, err
+	}
+	OptionsInterface := convertDhcpOptionsToInterface(obj.Options)
+	if err = d.Set("options", OptionsInterface); err != nil {
+		return nil, err
+	}
+	d.SetId(obj.Ref)
+
+	// Update the resource with the EA Terraform Internal ID
+	err = resourceFixedRecordUpdate(d, m)
+	if err != nil {
+		return nil, err
+	}
+	return []*schema.ResourceData{d}, nil
+}
+func ConvertInterfaceToDhcpOptions(optionsInterface []interface{}) []*ibclient.Dhcpoption {
+	var options []*ibclient.Dhcpoption
+	for _, optionInterface := range optionsInterface {
+		option := optionInterface.(map[string]interface{})
+		dhcpOption := &ibclient.Dhcpoption{
+			Name:        option["name"].(string),
+			Num:         uint32(option["num"].(int)),
+			VendorClass: option["vendor_class"].(string),
+			Value:       option["value"].(string),
+			UseOption:   option["use_option"].(bool),
+		}
+		options = append(options, dhcpOption)
+	}
+	return options
+}
+
+func convertDhcpOptionsToInterface(options []*ibclient.Dhcpoption) []map[string]interface{} {
+	optionsInterface := make([]map[string]interface{}, 0, len(options))
+	for _, option := range options {
+		optionMap := make(map[string]interface{})
+		optionMap["name"] = option.Name
+		optionMap["num"] = option.Num
+		optionMap["vendor_class"] = option.VendorClass
+		optionMap["value"] = option.Value
+		optionMap["use_option"] = option.UseOption
+		optionsInterface = append(optionsInterface, optionMap)
+	}
+	return optionsInterface
+}
