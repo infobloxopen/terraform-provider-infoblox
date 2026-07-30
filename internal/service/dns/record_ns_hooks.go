955e2c0502b4a9b8a397b28cb5b1e8dc836d542f
Chaithra007 <chaithra@infoblox.com>
2026-07-30 14:40:45 +0530

    added hooks changes for rdata

--- internal/service/dns/record_ns_hooks.go
+++ internal/service/dns/record_ns_hooks.go
@@ -3,7 +3,12 @@ package dns
 import (
 	"context"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
+	"github.com/hashicorp/terraform-plugin-framework/diag"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
+	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
+	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 	"github.com/infobloxopen/terraform-provider-infoblox/internal/flex"
 )
 
@@ -21,4 +26,47 @@ func validateRecordNsNIOSConfig(ctx context.Context, m *NIOSRecordNsModel, resp 
 }
 
 func validateRecordNsUDDIConfig(ctx context.Context, m *UDDIRecordNsModel, resp *resource.ValidateConfigResponse) {
+}
+
+type UDDIRecordNsRdataModel struct {
+	Dname types.String `tfsdk:"dname"`
+}
+
+var UDDIRecordNsRdataAttrTypes = map[string]attr.Type{
+	"dname": types.StringType,
+}
+
+var UDDIRecordNsRdataResourceSchemaAttributes = map[string]schema.Attribute{
+	"dname": schema.StringAttribute{
+		Required:            true,
+		MarkdownDescription: "The domain name of the authoritative name server for the zone.",
+	},
+}
+
+func ExpandUDDIRecordNsRdata(ctx context.Context, o types.Object, diags *diag.Diagnostics) map[string]any {
+	if o.IsNull() || o.IsUnknown() {
+		return nil
+	}
+	var m UDDIRecordNsRdataModel
+	diags.Append(o.As(ctx, &m, basetypes.ObjectAsOptions{})...)
+	if diags.HasError() {
+		return nil
+	}
+	rdata := make(map[string]any)
+	if dname := flex.ExpandString(m.Dname); dname != "" {
+		rdata["dname"] = dname
+	}
+	return rdata
+}
+
+func FlattenUDDIRecordNsRdata(ctx context.Context, from map[string]any, diags *diag.Diagnostics) types.Object {
+	if from == nil {
+		return types.ObjectNull(UDDIRecordNsRdataAttrTypes)
+	}
+	m := UDDIRecordNsRdataModel{
+		Dname: flex.FlattenStringPointer(flex.RDataStringPtr(from["dname"])),
+	}
+	obj, d := types.ObjectValueFrom(ctx, UDDIRecordNsRdataAttrTypes, m)
+	diags.Append(d...)
+	return obj
 }
