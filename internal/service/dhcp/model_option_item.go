2a71582924681dc2a76911c18defdac89bc3e6cc
hgupta-infoblox <hgupta1@infoblox.com>
2026-08-10 13:14:14 +0530

    added examples n docs

--- internal/service/dhcp/model_option_item.go
+++ internal/service/dhcp/model_option_item.go
@@ -56,13 +56,17 @@ var OptionItemResourceSchemaAttributes = map[string]schema.Attribute{
 	},
 	"option_value": schema.StringAttribute{
 		Optional: true,
+		Computed: true,
 		Validators: []validator.String{
 			stringvalidator.AlsoRequires(path.MatchRelative().AtParent().AtName("option_code")),
 		},
 		MarkdownDescription: "The option value.",
 	},
 	"type": schema.StringAttribute{
-		Optional:            true,
+		Optional: true,
+		Validators: []validator.String{
+			stringvalidator.OneOf("group", "option"),
+		},
 		MarkdownDescription: "The type of item.  Valid values are: * _group_ * _option_",
 	},
 }
