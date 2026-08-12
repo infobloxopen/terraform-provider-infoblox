774bff8110477e41af9e40dc7e5246985981bb06
VishrutiBuddhadev <vbuddhadev@infoblox.com>
2026-08-12 18:32:46 +0530

    fixed list descriptions

--- internal/service/dns/record_ns_list.go
+++ internal/service/dns/record_ns_list.go
@@ -69,7 +69,7 @@ func (l *RecordNsList) Configure(_ context.Context, req resource.ConfigureReques
 
 func (l *RecordNsList) ListResourceConfigSchema(_ context.Context, _ list.ListResourceSchemaRequest, resp *list.ListResourceSchemaResponse) {
 	resp.Schema = listschema.Schema{
-		MarkdownDescription: "Retrieves a list of Infoblox RecordNs from the configured backend (NIOS or UDDI).",
+		MarkdownDescription: "Retrieves a list of Infoblox RecordNs from both the NIOS and UDDI backends.",
 		Attributes: map[string]listschema.Attribute{
 			"filters": listschema.MapAttribute{
 				MarkdownDescription: "Filters are used to return a more specific list of results. Filters can be used to match resources by specific attributes (e.g. name, view). If multiple filters are specified, only resources that match all of them are returned.",
