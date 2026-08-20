88dbe4b337ca60cf9a8bc6507332c8e417f856bc
Aish-sp <apattar@infoblox.com>
2026-08-20 10:28:56 +0530

    Initial commit

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -287,6 +287,7 @@ func ensureNIOSPreRequisites(
 
 func (p *InfobloxProvider) Resources(_ context.Context) []func() resource.Resource {
 	return []func() resource.Resource{
+		dns.NewZoneForwardResource,
 		dns.NewRecordSrvResource,
 		dns.NewRecordNaptrResource,
 		dns.NewRecordMxResource,
@@ -310,6 +311,7 @@ func (p *InfobloxProvider) Resources(_ context.Context) []func() resource.Resour
 
 func (p *InfobloxProvider) DataSources(ctx context.Context) []func() datasource.DataSource {
 	return []func() datasource.DataSource{
+		dns.NewZoneForwardDataSource,
 		dns.NewRecordSrvDataSource,
 		dns.NewRecordNaptrDataSource,
 		dns.NewRecordMxDataSource,
@@ -336,6 +338,7 @@ func (p *InfobloxProvider) DataSources(ctx context.Context) []func() datasource.
 
 func (p *InfobloxProvider) ListResources(_ context.Context) []func() list.ListResource {
 	return []func() list.ListResource{
+		dns.NewZoneForwardList,
 		dns.NewRecordSrvList,
 		dns.NewRecordNaptrList,
 		dns.NewRecordMxList,
