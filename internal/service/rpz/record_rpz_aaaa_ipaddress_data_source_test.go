8c416f3062a5334e863449fa1651663179bb07ee
rsingh4 <rsingh4@infoblox.com>
2026-08-27 14:16:49 +0530

    renames tfvars  to hcl

--- internal/service/rpz/record_rpz_aaaa_ipaddress_data_source_test.go
+++ internal/service/rpz/record_rpz_aaaa_ipaddress_data_source_test.go
@@ -19,7 +19,7 @@ func TestAccRecordRpzAaaaIpaddressDataSource(t *testing.T) {
 
 	for _, backend := range []string{"nios"} {
 		t.Run(backend, func(t *testing.T) {
-			acctest.RunDataSourceCases(t, dsType, resourceType, "rpz/record_rpz_aaaa_ipaddress/"+backend+"_datasources.tfvars", checksByBackend)
+			acctest.RunDataSourceCases(t, dsType, resourceType, "rpz/record_rpz_aaaa_ipaddress/"+backend+"_datasources.hcl", checksByBackend)
 		})
 	}
 }
--- internal/service/rpz/record_rpz_aaaa_ipaddress_list_test.go
+++ internal/service/rpz/record_rpz_aaaa_ipaddress_list_test.go
@@ -18,7 +18,7 @@ func TestAccRecordRpzAaaaIpaddressList(t *testing.T) {
 
 	for _, backend := range []string{"nios"} {
 		t.Run(backend, func(t *testing.T) {
-			acctest.RunListCases(t, resourceType, "rpz/record_rpz_aaaa_ipaddress/"+backend+"_lists.tfvars", checksByBackend)
+			acctest.RunListCases(t, resourceType, "rpz/record_rpz_aaaa_ipaddress/"+backend+"_lists.hcl", checksByBackend)
 		})
 	}
 }
