19500e50ffba69a5aa8bcd12e4e0c4e8ee079763
Chaithra007 <chaithra@infoblox.com>
2026-08-28 13:21:44 +0530

    modified test data fo PTR record from tfvars file to hcl file

--- internal/service/dns/record_ptr_data_source_test.go
+++ internal/service/dns/record_ptr_data_source_test.go
@@ -23,7 +23,7 @@ func TestAccRecordPtrDataSource(t *testing.T) {
 
 	for _, backend := range []string{"nios", "uddi"} {
 		t.Run(backend, func(t *testing.T) {
-			acctest.RunDataSourceCases(t, dsType, resourceType, "dns/record_ptr/"+backend+"_datasources.tfvars", checksByBackend)
+			acctest.RunDataSourceCases(t, dsType, resourceType, "dns/record_ptr/"+backend+"_datasources.hcl", checksByBackend)
 		})
 	}
 }
--- internal/service/dns/record_ptr_resource_test.go
+++ internal/service/dns/record_ptr_resource_test.go
@@ -31,7 +31,7 @@ func TestAccRecordPtrResource(t *testing.T) {
 
 	for _, backend := range []string{"nios", "uddi"} {
 		t.Run(backend, func(t *testing.T) {
-			acctest.RunResourceCases(t, resourceType, "dns/record_ptr/"+backend+"_resources.tfvars", checksByBackend)
+			acctest.RunResourceCases(t, resourceType, "dns/record_ptr/"+backend+"_resources.hcl", checksByBackend)
 		})
 	}
 }
