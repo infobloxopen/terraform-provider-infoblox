a524eb4cbd3a7ac73e13f26af7d03d1ed90d587e
Chaithra007 <chaithra@infoblox.com>
2026-08-28 12:28:24 +0530

    minor changes

--- internal/service/dtc/model_dtc_lbdn.go
+++ internal/service/dtc/model_dtc_lbdn.go
@@ -123,7 +123,6 @@ var DtcLbdnResourceNiosSchemaAttributes = map[string]schema.Attribute{
 	"auth_zones": schema.ListAttribute{
 		ElementType: types.StringType,
 		Optional:    true,
-		Computed:    true,
 		Validators: []validator.List{
 			customvalidator.ListNotEmpty(),
 		},
@@ -225,7 +224,6 @@ var DtcLbdnResourceNiosSchemaAttributes = map[string]schema.Attribute{
 	},
 	"ttl": schema.Int64Attribute{
 		Optional:            true,
-		Computed:            true,
 		MarkdownDescription: "The Time To Live (TTL) value for the DTC LBDN. A 32-bit unsigned integer that represents the duration, in seconds, for which the record is valid (cached). Zero indicates that the record should not be cached.",
 	},
 	"types": schema.ListAttribute{
