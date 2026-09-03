b96f67e2990b6cc010369c839ed90919c59e52e2
Vishruti Buddhadev <vbuddhadev@infoblox.com>
2026-09-03 16:51:05 +0530

    [ALL] Regenerate Object Groups and Fix Miscellaneous Drift (#606)
    * regenerated all the existing object groups
    
    * regenerated docs

--- internal/service/misc/ruleset_resource.go
+++ internal/service/misc/ruleset_resource.go
@@ -5,15 +5,18 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/path"
 	"github.com/hashicorp/terraform-plugin-framework/resource"
 	"github.com/hashicorp/terraform-plugin-framework/resource/identityschema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/types"
 
 	"github.com/infobloxopen/terraform-provider-infoblox/internal/core"
 	coremodel "github.com/infobloxopen/terraform-provider-infoblox/internal/core/model/misc"
 	coresvc "github.com/infobloxopen/terraform-provider-infoblox/internal/core/service/misc"
 	"github.com/infobloxopen/terraform-provider-infoblox/internal/retry"
+	"github.com/infobloxopen/terraform-provider-infoblox/internal/validator"
 )
 
 var (
@@ -88,6 +91,12 @@ func (r *RulesetResource) ValidateConfig(ctx context.Context, req resource.Valid
 	var data RulesetModel
 
 	resp.Diagnostics.Append(req.Config.Get(ctx, &data)...)
+	if resp.Diagnostics.HasError() {
+		return
+	}
+
+	// Common backend block validations
+	validator.ValidateBackendBlocks(r.backend, data.NIOS, types.ObjectNull(map[string]attr.Type{}), &resp.Diagnostics)
 	if resp.Diagnostics.HasError() {
 		return
 	}
