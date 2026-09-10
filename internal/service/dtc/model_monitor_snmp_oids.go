ffb5c37bd256f47cfcce03f60e342de38f5816a6
Chaithra007 <chaithra@infoblox.com>
2026-09-10 15:42:05 +0530

    fixed oid issue

--- internal/service/dtc/model_monitor_snmp_oids.go
+++ internal/service/dtc/model_monitor_snmp_oids.go
@@ -7,6 +7,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
+	"github.com/hashicorp/terraform-plugin-framework/resource/schema/stringdefault"
 	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
@@ -53,6 +54,7 @@ var MonitorSnmpOidsResourceSchemaAttributes = map[string]schema.Attribute{
 		MarkdownDescription: "The comment for a DTC SNMP Health Monitor OID object.",
 	},
 	"type": schema.StringAttribute{
+		Default: stringdefault.StaticString("STRING"),
 		Validators: []validator.String{
 			stringvalidator.OneOf("STRING", "INTEGER"),
 		},
