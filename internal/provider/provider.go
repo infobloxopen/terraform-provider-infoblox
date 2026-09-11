fcaf442f8a55c5dec6d7902966e6c9be1cb7f4b8
Rishabh Singh <126642800+rsingh4infoblox@users.noreply.github.com>
2026-09-11 14:23:07 +0530

    Apply batched suggestions from code review
    Co-authored-by: Rishabh Singh <126642800+rsingh4infoblox@users.noreply.github.com>

--- internal/provider/provider.go
+++ internal/provider/provider.go
@@ -407,7 +407,7 @@ func (p *InfobloxProvider) Resources(_ context.Context) []func() resource.Resour
 		rpz.NewRecordRpzCnameIpaddressResource,
 		rpz.NewRecordRpzNaptrResource,
 		rpz.NewRecordRpzTxtResource,
-    rpz.NewRecordRpzAIpaddressResource,
+		rpz.NewRecordRpzAIpaddressResource,
 	}
 }
 
@@ -493,7 +493,7 @@ func (p *InfobloxProvider) DataSources(ctx context.Context) []func() datasource.
 		rpz.NewRecordRpzCnameIpaddressDataSource,
 		rpz.NewRecordRpzNaptrDataSource,
 		rpz.NewRecordRpzTxtDataSource,
-    rpz.NewRecordRpzAIpaddressDataSource,
+		rpz.NewRecordRpzAIpaddressDataSource,
 	}
 }
 
@@ -576,7 +576,7 @@ func (p *InfobloxProvider) ListResources(_ context.Context) []func() list.ListRe
 		rpz.NewRecordRpzCnameIpaddressList,
 		rpz.NewRecordRpzNaptrList,
 		rpz.NewRecordRpzTxtList,
-    rpz.NewRecordRpzAIpaddressList,
+		rpz.NewRecordRpzAIpaddressList,
 	}
 }
 
