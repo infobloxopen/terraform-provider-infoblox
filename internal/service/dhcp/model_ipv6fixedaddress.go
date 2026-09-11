9a4ed07a0488bdadb71b21d331880dee8313390e
VishrutiBuddhadev <vbuddhadev@infoblox.com>
2026-09-11 16:04:06 +0530

    fix NPA-3189

--- internal/service/dhcp/model_ipv6fixedaddress.go
+++ internal/service/dhcp/model_ipv6fixedaddress.go
@@ -266,6 +266,7 @@ var Ipv6fixedaddressResourceNiosSchemaAttributes = map[string]schema.Attribute{
 	"domain_name_servers": schema.ListAttribute{
 		ElementType: types.StringType,
 		Optional:    true,
+		Computed:    true,
 		Validators: []validator.List{
 			customvalidator.ListNotEmpty(),
 			listvalidator.ValueStringsAre(customvalidator.IsValidIPv6Address()),
