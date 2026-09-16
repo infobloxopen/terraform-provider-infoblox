8cc0d1d95796f4fdfd73e7167ff17049e1b3f5c3
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2026-09-16 16:31:33 +0530

    resolved dependency

--- internal/acctest/cases.go
+++ internal/acctest/cases.go
@@ -11,7 +11,6 @@ import (
 	"github.com/hashicorp/hcl/v2"
 	"github.com/hashicorp/hcl/v2/hclparse"
 	"github.com/hashicorp/hcl/v2/hclsyntax"
-	"github.com/zclconf/go-cty/cty"
 	"github.com/hashicorp/terraform-plugin-testing/helper/resource"
 	"github.com/zclconf/go-cty/cty"
 
