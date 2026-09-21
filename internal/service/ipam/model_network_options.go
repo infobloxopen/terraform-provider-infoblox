186c56375da667f625ddc4e0b32ef35e9fbb1985
hgupta-infoblox <hgupta1@infoblox.com>
2026-09-21 13:41:53 +0530

    removed optional

--- internal/service/ipam/model_network_options.go
+++ internal/service/ipam/model_network_options.go
@@ -68,7 +68,6 @@ var NetworkOptionsResourceSchemaAttributes = map[string]schema.Attribute{
 		MarkdownDescription: "Value of the DHCP option",
 	},
 	"use_option": schema.BoolAttribute{
-		Optional:            true,
 		Computed:            true,
 		MarkdownDescription: "Only applies to special options that are displayed separately from other options and have a use flag. These options are: * routers * router-templates * domain-name-servers * domain-name * broadcast-address * broadcast-address-offset * dhcp-lease-time * dhcp6.name-servers",
 	},
