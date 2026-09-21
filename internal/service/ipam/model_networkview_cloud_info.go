8f74ff2be75a5dab2fcf93773c110be532029922
hgupta-infoblox <hgupta1@infoblox.com>
2026-09-21 11:24:39 +0530

    genarted file

--- internal/service/ipam/model_networkview_cloud_info.go
+++ internal/service/ipam/model_networkview_cloud_info.go
@@ -3,41 +3,23 @@ package ipam
 import (
 	"context"
 
-	"github.com/hashicorp/terraform-plugin-framework-validators/stringvalidator"
 	"github.com/hashicorp/terraform-plugin-framework/attr"
 	"github.com/hashicorp/terraform-plugin-framework/diag"
 	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
-	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
 
 	niosipam "github.com/infobloxopen/infoblox-nios-go-client/ipam"
-	"github.com/infobloxopen/terraform-provider-infoblox/internal/flex"
-	customvalidator "github.com/infobloxopen/terraform-provider-infoblox/internal/validator"
 )
 
 // NetworkviewCloudInfoModel is the Terraform model for NetworkviewCloudInfo
 type NetworkviewCloudInfoModel struct {
 	DelegatedMember types.Object `tfsdk:"delegated_member"`
-	DelegatedScope  types.String `tfsdk:"delegated_scope"`
-	DelegatedRoot   types.String `tfsdk:"delegated_root"`
-	OwnedByAdaptor  types.Bool   `tfsdk:"owned_by_adaptor"`
-	Usage           types.String `tfsdk:"usage"`
-	Tenant          types.String `tfsdk:"tenant"`
-	MgmtPlatform    types.String `tfsdk:"mgmt_platform"`
-	AuthorityType   types.String `tfsdk:"authority_type"`
 }
 
 // NetworkviewCloudInfoAttrTypes contains the attribute types for NetworkviewCloudInfoModel
 var NetworkviewCloudInfoAttrTypes = map[string]attr.Type{
 	"delegated_member": types.ObjectType{AttrTypes: NetworkviewcloudinfoDelegatedMemberAttrTypes},
-	"delegated_scope":  types.StringType,
-	"delegated_root":   types.StringType,
-	"owned_by_adaptor": types.BoolType,
-	"usage":            types.StringType,
-	"tenant":           types.StringType,
-	"mgmt_platform":    types.StringType,
-	"authority_type":   types.StringType,
 }
 
 // NetworkviewCloudInfoResourceSchemaAttributes contains the schema attributes for NetworkviewCloudInfoModel
@@ -47,52 +29,6 @@ var NetworkviewCloudInfoResourceSchemaAttributes = map[string]schema.Attribute{
 		Optional:            true,
 		Computed:            true,
 		MarkdownDescription: "The Cloud Platform Appliance to which authority of the object is delegated.",
-	},
-	"delegated_scope": schema.StringAttribute{
-		Validators: []validator.String{
-			stringvalidator.OneOf("NONE", "ROOT", "SUBTREE", "RECLAIMING"),
-		},
-		Computed:            true,
-		MarkdownDescription: "Indicates the scope of delegation for the object. This can be one of the following: NONE (outside any delegation), ROOT (the delegation point), SUBTREE (within the scope of a delegation), RECLAIMING (within the scope of a delegation being reclaimed, either as the delegation point or in the subtree).",
-	},
-	"delegated_root": schema.StringAttribute{
-		Computed: true,
-		Validators: []validator.String{
-			customvalidator.StringNotEmpty(),
-		},
-		MarkdownDescription: "Indicates the root of the delegation if delegated_scope is SUBTREE or RECLAIMING. This is not set otherwise.",
-	},
-	"owned_by_adaptor": schema.BoolAttribute{
-		Computed:            true,
-		MarkdownDescription: "Determines whether the object was created by the cloud adapter or not.",
-	},
-	"usage": schema.StringAttribute{
-		Validators: []validator.String{
-			stringvalidator.OneOf("NONE", "ADAPTER", "USED_BY", "DELEGATED"),
-		},
-		Computed:            true,
-		MarkdownDescription: "Indicates the cloud origin of the object.",
-	},
-	"tenant": schema.StringAttribute{
-		Computed: true,
-		Validators: []validator.String{
-			customvalidator.StringNotEmpty(),
-		},
-		MarkdownDescription: "Reference to the tenant object associated with the object, if any.",
-	},
-	"mgmt_platform": schema.StringAttribute{
-		Computed: true,
-		Validators: []validator.String{
-			customvalidator.StringNotEmpty(),
-		},
-		MarkdownDescription: "Indicates the specified cloud management platform.",
-	},
-	"authority_type": schema.StringAttribute{
-		Validators: []validator.String{
-			stringvalidator.OneOf("NONE", "GM", "CP"),
-		},
-		Computed:            true,
-		MarkdownDescription: "Type of authority over the object.",
 	},
 }
 
@@ -138,11 +74,4 @@ func (m *NetworkviewCloudInfoModel) Flatten(ctx context.Context, from *niosipam.
 		return
 	}
 	m.DelegatedMember = FlattenNetworkviewcloudinfoDelegatedMember(ctx, from.DelegatedMember, diags)
-	m.DelegatedScope = flex.FlattenStringPointerEmptyAsNull(from.DelegatedScope)
-	m.DelegatedRoot = flex.FlattenStringPointerEmptyAsNull(from.DelegatedRoot)
-	m.OwnedByAdaptor = flex.FlattenBoolPointer(from.OwnedByAdaptor)
-	m.Usage = flex.FlattenStringPointerEmptyAsNull(from.Usage)
-	m.Tenant = flex.FlattenStringPointerEmptyAsNull(from.Tenant)
-	m.MgmtPlatform = flex.FlattenStringPointerEmptyAsNull(from.MgmtPlatform)
-	m.AuthorityType = flex.FlattenStringPointerEmptyAsNull(from.AuthorityType)
 }
