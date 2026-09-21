61b460c8ffca155ebb7b2b3a5980e5c3506c3489
Ujjwal Nasra <125353741+unasra@users.noreply.github.com>
2026-09-21 13:41:52 +0530

    Added Changes for SVCB and HTTPS Hooks

--- internal/service/dns/record_svcb_hooks.go
+++ internal/service/dns/record_svcb_hooks.go
@@ -11,10 +11,13 @@ import (
 	schema "github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/int64default"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema/listdefault"
+	"github.com/hashicorp/terraform-plugin-framework/schema/validator"
 	"github.com/hashicorp/terraform-plugin-framework/types"
 	"github.com/hashicorp/terraform-plugin-framework/types/basetypes"
+
 	"github.com/infobloxopen/terraform-provider-infoblox/internal/flex"
 	"github.com/infobloxopen/terraform-provider-infoblox/internal/utils"
+	customvalidator "github.com/infobloxopen/terraform-provider-infoblox/internal/validator"
 )
 
 // ValidateRecordSvcb validates the RecordSvcb configuration.
@@ -48,15 +51,23 @@ func validateRecordSvcbUDDIConfig(ctx context.Context, m *UDDIRecordSvcbModel, r
 			if p.Key.IsNull() || p.Key.IsUnknown() {
 				continue
 			}
+			paramPath := path.Root("uddi").AtName("rdata").AtName("svc_params").AtListIndex(i).AtName("value")
 			if p.Key.ValueString() == "ohttp" {
-				continue
-			}
-			if p.Value.IsNull() {
-				resp.Diagnostics.AddAttributeError(
-					path.Root("uddi").AtName("rdata").AtName("svc_params").AtListIndex(i).AtName("value"),
-					"Invalid Configuration",
-					fmt.Sprintf("'value' is required for svc_params entry with key %q.", p.Key.ValueString()),
-				)
+				if !p.Value.IsNull() {
+					resp.Diagnostics.AddAttributeError(
+						paramPath,
+						"Invalid Configuration",
+						"'value' must not be provided for svc_params entry with key \"ohttp\".",
+					)
+				}
+			} else {
+				if p.Value.IsNull() {
+					resp.Diagnostics.AddAttributeError(
+						paramPath,
+						"Invalid Configuration",
+						fmt.Sprintf("'value' is required for svc_params entry with key %q.", p.Key.ValueString()),
+					)
+				}
 			}
 		}
 	}
@@ -90,7 +101,11 @@ var UDDIRecordSvcbSvcParamResourceSchemaAttributes = map[string]schema.Attribute
 		MarkdownDescription: "The service parameter key (e.g. \"port\", \"ipv4hint\", \"ipv6hint\", \"ech\", \"alpn\").",
 	},
 	"value": schema.StringAttribute{
-		Optional:            true,
+		Optional: true,
+		Validators: []validator.String{
+			customvalidator.StringNotEmpty(),
+			customvalidator.ValidateTrimmedString(),
+		},
 		MarkdownDescription: "The service parameter value.",
 	},
 }
