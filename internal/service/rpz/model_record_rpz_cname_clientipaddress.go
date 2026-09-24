9a98e8989690a7b64792571619885c2ecc5ada52
rsingh4 <rsingh4@infoblox.com>
2026-09-24 11:45:44 +0530

    updated the trigger_update

--- internal/service/rpz/model_record_rpz_cname_clientipaddress.go
+++ internal/service/rpz/model_record_rpz_cname_clientipaddress.go
@@ -25,13 +25,15 @@ import (
 )
 
 type RecordRpzCnameClientipaddressModel struct {
-	Id   types.String `tfsdk:"id"`
-	NIOS types.Object `tfsdk:"nios"`
+	Id            types.String `tfsdk:"id"`
+	UpdateTrigger types.String `tfsdk:"update_trigger"`
+	NIOS          types.Object `tfsdk:"nios"`
 }
 
 var RecordRpzCnameClientipaddressAttrTypes = map[string]attr.Type{
-	"id":   types.StringType,
-	"nios": types.ObjectType{AttrTypes: NIOSRecordRpzCnameClientipaddressAttrTypes},
+	"id":             types.StringType,
+	"update_trigger": types.StringType,
+	"nios":           types.ObjectType{AttrTypes: NIOSRecordRpzCnameClientipaddressAttrTypes},
 }
 
 type NIOSRecordRpzCnameClientipaddressModel struct {
@@ -66,6 +68,10 @@ var RecordRpzCnameClientipaddressResourceSchemaAttributes = map[string]schema.At
 	"id": schema.StringAttribute{
 		Computed:            true,
 		MarkdownDescription: "The reference to the object.",
+	},
+	"update_trigger": schema.StringAttribute{
+		Optional:            true,
+		MarkdownDescription: "An arbitrary value used to trigger an update. Not sent to the API. Change it when Terraform reports no infrastructure changes.",
 	},
 	"nios": schema.SingleNestedAttribute{
 		Optional:            true,
