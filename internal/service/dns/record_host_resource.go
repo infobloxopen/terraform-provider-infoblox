a84f51e7a56318c02d45ad9848981a1af54465b3
Raghav Sharma <rsharma6@infoblox.com>
2026-09-25 11:05:58 +0530

    Fixed the import section

--- internal/service/dns/record_host_resource.go
+++ internal/service/dns/record_host_resource.go
@@ -11,6 +11,7 @@ import (
 	"github.com/hashicorp/terraform-plugin-framework/resource/identityschema"
 	"github.com/hashicorp/terraform-plugin-framework/resource/schema"
 	"github.com/hashicorp/terraform-plugin-framework/types"
+
 	"github.com/infobloxopen/terraform-provider-infoblox/internal/core"
 	coremodel "github.com/infobloxopen/terraform-provider-infoblox/internal/core/model/dns"
 	coresvc "github.com/infobloxopen/terraform-provider-infoblox/internal/core/service/dns"
